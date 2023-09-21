6b87fa867a6621ab4b91714e980b1e4af085d7e0
Ryan Lerch <rlerch@redhat.com>
2023-09-21 21:59:26 +1000

    add oncall feature
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

---
+++ fedora/db.py
@@ -0,0 +1,14 @@
+from mautrix.util.async_db import UpgradeTable, Connection
+
+upgrade_table = UpgradeTable()
+
+
+@upgrade_table.register(description="Initial revision")
+async def upgrade_v1(conn: Connection) -> None:
+    await conn.execute(
+        """CREATE TABLE oncall (
+         username TEXT PRIMARY KEY,
+         mxid TEXT NOT NULL,
+         timezone TEXT NOT NULL
+    )"""
+    )
