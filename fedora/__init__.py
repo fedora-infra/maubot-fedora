539def557f5bdf1dc17108b1ac814be9233703f2
Ryan Lerch <rlerch@redhat.com>
2023-09-26 17:17:42 +1000

    add the !bug command
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- fedora/__init__.py
+++ fedora/__init__.py
@@ -12,6 +12,7 @@ from asyncpg.exceptions import UniqueViolationError
 
 from .clients.fasjson import FasjsonClient
 from .clients.pagure import PagureClient
+from .clients.bugzilla import BugzillaClient
 
 from .exceptions import InfoGatherError
 from .db import upgrade_table
@@ -125,6 +126,8 @@ class Fedora(Plugin):
         self.pagureioclient = PagureClient(self.config["pagureio_url"])
         self.paguredistgitclient = PagureClient(self.config["paguredistgit_url"])
         self.fasjsonclient = FasjsonClient(self.config["fasjson_url"])
+        self.bugzillaclient = BugzillaClient("https://bugzilla.redhat.com")
+
 
     async def stop(self) -> None:
         pass
@@ -486,6 +489,19 @@ class Fedora(Plugin):
         resp = "".join([x for x in [owners, admins, committers] if x != ""])
 
         await evt.respond(resp)
+
+    @command.new(help="return a bugzilla bug")
+    @command.argument("bug_id", required=True)
+    async def bug(self, evt: MessageEvent, bug_id: str) -> None:
+        if not bug_id:
+            await evt.respond(
+                "bug_id argument is required. e.g. `!bug 1234567`"
+            )
+            return
+        result = await self.bugzillaclient.get_bug(bug_id)
+        json = result[0].json()
+        await evt.respond(f"[RHBZ#{bug_id}](https://bugzilla.redhat.com/{bug_id}): {json['bugs'][0]['summary']}")
+
 
     @command.new(help="return a pagure issue")
     @command.argument("project", required=True)
---
+++ fedora/clients/bugzilla.py
@@ -0,0 +1,28 @@
+import httpx
+
+from ..exceptions import InfoGatherError
+
+
+class BugzillaClient:
+    def __init__(self, baseurl):
+        self.baseurl = f"{baseurl}/rest/"
+
+    async def _get(self, endpoint, **kwargs):
+        async with httpx.AsyncClient() as client:
+            response = await client.get(self.baseurl + endpoint, **kwargs)
+        return response
+
+    def _check_errors(self, response):
+        if response.status_code == 404:
+            raise InfoGatherError(
+                f"Issue querying Pagure: {response.json().get('error')}"
+            )
+        elif response.status_code != 200:
+            raise InfoGatherError(
+                f"Issue querying Pagure: {response.status_code}: {response.reason_phrase}"
+            )
+
+    async def get_bug(self, bug_id):
+        response = await self._get("/".join(["bug", bug_id])),
+        #self._check_errors(response)
+        return response
