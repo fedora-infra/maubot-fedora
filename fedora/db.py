24de2af51391bd2437afaa165488020a53c34f79
Aurélien Bompard <aurelien@bompard.org>
2023-10-05 18:35:34 +0200

    Minor DB refactor
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fedora/db.py
+++ fedora/db.py
@@ -1,4 +1,9 @@
+from sqlite3 import IntegrityError
+
+from asyncpg.exceptions import UniqueViolationError
 from mautrix.util.async_db import Connection, UpgradeTable
+
+UNIQUE_ERROR = (UniqueViolationError, IntegrityError)
 
 upgrade_table = UpgradeTable()
 
