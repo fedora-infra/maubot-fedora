232f160509c4838d29f5d0545269bfd2640dd688
Ryan Lerch <rlerch@redhat.com>
2023-10-25 13:14:44 +1000

    remove old code that documented aliases
    we don't use maubot aliases anymore, so remove the code that showed the
    aliases in the help
    
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- fedora/constants.py
+++ fedora/constants.py
@@ -1,7 +1,6 @@
 import re
 
 NL = "      \n"
-ALIASES: dict[str, list[str]] = {}
 MATRIX_USER_RE = re.compile(r"@([^:]+):([^\s]+)")
 COMMAND_RE = re.compile(r"^!(\S+)(?:\s+|$)(.*)")
 # Domains where we can safely use the username as a FAS user
