693f825e1e3f304f411883fbb25c38dcfc4457d2
Ryan Lerch <rlerch@redhat.com>
2023-12-07 15:40:20 +1000

    return more information on pagure commands
    added additional information to pagure ticket commands, including
    assignee, ticket close status, and dates of opening, modification,
    and closure.
    
    Signed-off-by: Ryan Lerch <rlerch@redhat.com>

--- tests/test_pagure.py
+++ tests/test_pagure.py
@@ -1,3 +1,4 @@
+import datetime
 from unittest import mock
 
 import httpx
@@ -16,24 +17,94 @@ import fedora
     ],
 )
 async def test_pagureissue(bot, plugin, respx_mock, command, project):
+    user = {
+        "full_url": "https://pagure.io/user/humaton",
+        "fullname": "Tomáš Hrčka",
+        "name": "humaton",
+        "url_path": "user/humaton",
+    }
+    two_weeks_ago_ts = int((datetime.datetime.utcnow() + datetime.timedelta(weeks=-2)).timestamp())
+    one_week_ago_ts = int((datetime.datetime.utcnow() + datetime.timedelta(weeks=-1)).timestamp())
+    response = {
+        "assignee": None,
+        "blocks": [],
+        "close_status": None,
+        "closed_at": None,
+        "closed_by": None,
+        "comments": [],
+        "content": "Hi, \r\nafter the epel9 rwas created a",
+        "custom_fields": [],
+        "date_created": two_weeks_ago_ts,
+        "depends": [],
+        "full_url": f"https://pagure.io/{project}/issue/261",
+        "id": 261,
+        "last_updated": two_weeks_ago_ts,
+        "milestone": None,
+        "priority": None,
+        "private": False,
+        "related_prs": [],
+        "status": "Open",
+        "tags": [],
+        "title": "When creating new epel release please include MDAPI",
+        "user": user,
+    }
     respx_mock.get(f"http://pagure.example.com/api/0/{project}/issue/42").mock(
         return_value=httpx.Response(
             200,
-            json={
-                "title": "Dummy Issue",
-                "full_url": f"http://pagure.example.com/{project}/issues/42",
-            },
+            json=response,
         )
     )
     await bot.send(f"!{command} 42")
     assert len(bot.sent) == 1
-    expected_text = f"{project} #42 (http://pagure.example.com/{project}/issues/42): Dummy Issue"
-    expected_html = (
-        f'<p><a href="http://pagure.example.com/{project}/issues/42">'
-        f"{project} #42</a>: Dummy Issue</p>\n"
+    assert bot.sent[0].content.body == (
+        f"**{project} #42** (https://pagure.io/{project}/issue/261):"
+        f"**When creating new epel release please include MDAPI**\n\n"
+        f"● **Opened:** 2 weeks ago by humaton\n"
+        f"● **Last Updated:** Never\n"
+        f"● **Assignee:** Not Assigned"
     )
-    assert bot.sent[0].content.body == expected_text
-    assert bot.sent[0].content.formatted_body == expected_html
+    assert bot.sent[0].content.formatted_body == (
+        f'<p><a href="https://pagure.io/{project}/issue/261">'
+        f"<strong>{project} #42</strong></a>:"
+        f"<strong>When creating new epel release please include MDAPI</strong>"
+        f"</p>\n<ul>\n"
+        f"<li><strong>Opened:</strong> 2 weeks ago by humaton</li>\n"
+        f"<li><strong>Last Updated:</strong> Never</li>\n"
+        f"<li><strong>Assignee:</strong> Not Assigned</li>\n</ul>\n"
+    )
+
+    # test an issue that is closed and assigned
+    response["closed_at"] = one_week_ago_ts
+    response["closed_by"] = user
+    response["close_status"] = "Closed"
+    response["last_updated"] = one_week_ago_ts
+    response["assignee"] = user
+    respx_mock.get(f"http://pagure.example.com/api/0/{project}/issue/42").mock(
+        return_value=httpx.Response(
+            200,
+            json=response,
+        )
+    )
+    await bot.send(f"!{command} 42")
+    assert len(bot.sent) == 2
+    assert bot.sent[1].content.body == (
+        f"**{project} #42** (https://pagure.io/{project}/issue/261):"
+        f"**When creating new epel release please include MDAPI**\n\n"
+        f"● **Open: Closed** a week ago by humaton\n"
+        f"● **Opened:** 2 weeks ago by humaton\n"
+        f"● **Last Updated:** a week ago\n"
+        f"● **Assignee:** humaton"
+    )
+    assert bot.sent[1].content.formatted_body == (
+        f'<p><a href="https://pagure.io/{project}/issue/261">'
+        f"<strong>{project} #42</strong></a>:"
+        f"<strong>When creating new epel release please include MDAPI</strong></p>\n"
+        f"<ul>\n"
+        f"<li><strong>Open: Closed</strong> a week ago by humaton</li>\n"
+        f"<li><strong>Opened:</strong> 2 weeks ago by humaton</li>\n"
+        f"<li><strong>Last Updated:</strong> a week ago</li>\n"
+        f"<li><strong>Assignee:</strong> humaton</li>\n</ul>\n"
+    )
 
 
 async def test_issue_notfound(bot, plugin, respx_mock):
