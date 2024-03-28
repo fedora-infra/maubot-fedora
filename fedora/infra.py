09474bae02559aeb95ddefd65c1b178f9c5f71c0
KimFarida <farimomoh@gmail.com>
2024-03-28 05:25:23 +0100

    Fix: Explicit Timezone Null Check for user oncall

--- fedora/infra.py
+++ fedora/infra.py
@@ -101,7 +101,10 @@ class InfraHandler(Handler):
             # Just grab the first mxid in the users list on fas
             mxid = mxids[0]
         try:
-            await self.plugin.database.execute(dbq, fasusername, mxid, user.get("timezone", "UTC"))
+            timezone_to_insert = user.get("timezone")
+            if timezone_to_insert is None:
+                timezone_to_insert = "UTC"
+            await self.plugin.database.execute(dbq, fasusername, mxid, timezone_to_insert)
         except UNIQUE_ERROR:
             await evt.respond(f"{fasusername} is already on the oncall list")
             return
