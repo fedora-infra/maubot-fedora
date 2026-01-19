8d304d59068a1d6be785db09fd45c1203fc3eff7
Aurélien Bompard <aurelien@bompard.org>
2026-01-19 09:40:37 +0100

    Fix formatting with black
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- fedora/db.py
+++ fedora/db.py
@@ -10,19 +10,16 @@ upgrade_table = UpgradeTable()
 
 @upgrade_table.register(description="Initial revision")  # type: ignore
 async def upgrade_v1(conn: Connection) -> None:
-    await conn.execute(
-        """CREATE TABLE oncall (
+    await conn.execute("""CREATE TABLE oncall (
          username TEXT PRIMARY KEY,
          mxid TEXT NOT NULL,
          timezone TEXT NOT NULL
-    )"""
-    )
+    )""")
 
 
 @upgrade_table.register(description="Add cookies table")  # type: ignore
 async def upgrade_v2(conn: Connection) -> None:
-    await conn.execute(
-        """
+    await conn.execute("""
         CREATE TABLE cookies (
             from_user VARCHAR(254) NOT NULL,
             to_user VARCHAR(254) NOT NULL,
@@ -31,19 +28,14 @@ async def upgrade_v2(conn: Connection) -> None:
             date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
             PRIMARY KEY (from_user, to_user, release)
         )
-    """
-    )
-    await conn.execute(
-        """
+    """)
+    await conn.execute("""
         CREATE INDEX idx_cookies_to_user_release ON cookies (to_user, release);
-    """
-    )
+    """)
 
 
 @upgrade_table.register(description="Add index on cookie.to_user")  # type: ignore
 async def upgrade_v3(conn: Connection) -> None:
-    await conn.execute(
-        """
+    await conn.execute("""
         CREATE INDEX idx_cookies_to_user ON cookies (to_user);
-    """
-    )
+    """)
